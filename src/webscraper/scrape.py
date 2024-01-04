292738ec9e4db3b2101ea4916677f1cdbd836d55
landreesen <landreesen@uchicago.edu>
2024-01-04 09:57:40 -0600

    Improved error/response code handling

--- src/webscraper/scrape.py
+++ src/webscraper/scrape.py
@@ -8,6 +8,7 @@ import csv
 import datetime
 import time
 import logging
+import requests
 
 from processor import Processor
 from smart_queue import SmartQueue
@@ -21,6 +22,7 @@ S3_PROFILE = 'S3-website-crawler-550716682872'
 S3_SESSION = boto3.Session(profile_name = S3_PROFILE)
 BUCKET_NAME = 'website-crawler'
 
+RETRY_CODES = [408, 409, 413, 415, 417, 421, 451]
 
 
 def get_message():
@@ -32,6 +34,47 @@ def get_message():
 
     for message in sqs_queue.receive_messages(MessageAttributeNames=['All']):
         return message
+
+def get_metadata(domain, metadata):
+    current_datetime = datetime.datetime.now()
+    formatted_date = current_datetime.strftime("%Y-%m-%d")
+    s3 = S3_SESSION.resource('s3')
+    try:
+        # Object found in success directory
+        object = s3.Object(BUCKET_NAME, ('metadata/success' + domain + '.json'))
+        data = object.get()
+        json_string = json.loads(json.loads(['Body'].read().decode('utf-8')))
+        json_string[formatted_date] = metadata
+        return (json_string, 'success')
+    except:
+        try:
+        # Object found in low_count directory
+            object = s3.Object(BUCKET_NAME, ('metadata/failure' + domain + '.json'))
+            data = object.get()
+            json_string = json.loads(json.loads(['Body'].read().decode('utf-8')))
+            json_string[formatted_date] = metadata
+            return (json_string, 'fail')
+        except:
+            try:
+            # Object found in failure directory
+                object = s3.Object(BUCKET_NAME, ('metadata/low_count' + domain + '.json'))
+                data = object.get()
+                json_string = json.loads(json.loads(['Body'].read().decode('utf-8')))
+                json_string[formatted_date] = metadata
+                return (json_string, 'low_count')
+            except:
+                json_string = {formatted_date: metadata}
+
+def get_attempts():
+    s3 = S3_SESSION.resource('s3')                                              
+    bucket = s3.Bucket(BUCKET_NAME) 
+# check metadata/success and metadata/failure, if not in either, attempts = 0
+# otherwise, attempts = attempts + 1 
+
+def retry(code):
+    if (int(code) >= 500) or (int(code) in RETRY_CODES):
+      return True
+    return False
 
 def main(sqs_name, number_links):
     # Create loggers for standard output and error
@@ -68,19 +111,22 @@ def main(sqs_name, number_links):
     try:
         with open(process_file, 'w') as log_file:
             with open(error_file, 'w') as error_file:
-                #sys.stdout = log_file
-            #  sys.stderr = error_file
-             #   reader = csv.reader(sys.stdin)
+#      save          sys.stdout = log_file
+# save     sys.stderr = error_file
                 link_count = 0
-             #   while(True):
+                tst = s3.Object(BUCKET_NAME, 'html_data/360advanced.com.json')
+                print(tst["domain"])
                 while(link_count < number_links):
+                    url = ''
+                    message = None
+                    status = "success"
                     try:
                         message = get_message()
-                         
                         url = 'http://' + message.message_attributes['domain']['StringValue']
-                        #print(url)
+                        response = requests.get(url)
+                        code = response.status_code
                         f = open(master_file, "w")
-                        f.write(url)
+                        f.write(url + ',')
                         f.write(',')
                         f.close
 
@@ -97,30 +143,80 @@ def main(sqs_name, number_links):
                                 check_files.append(queue.root)
                             p = Processor(queue)#, results_file)
                             text = p.get_text()
+                            
                             data = {
                                     "domain": queue.root,
                                     "date": formatted_datetime,
                                     "count": queue.total_words, #this will need to be updated
                                     "html": text
-                                    }
+                            }
+                            error_data = {
+                                        "url": url,
+                                        "date": formatted_datetime,
+                                        "response_code": code
+                            }
                             json_data = json.dumps(data, indent = 2)
-                          #  with open(results_file, 'w') as file:
-                           #     file.write(json_data)
-                            bucket.put_object(Body = json.dumps(json_data), Key = ('html_data/' + queue.home_domain + '.json'))
-
-                       #     sqs = boto3.resource('sqs')
-                        ##    sqs_queue = sqs.get_queue_by_name(QueueName=SQS_)
-                         #   s3 = boto3.resource('s3')
-                        #    bucket = s3.Bucket('website-crawler')
+# Case 1: No words found 
+                            if queue.total_words == 0:
+                                status = "fail"
+                                json_error_data = json.dumps(error_data, indent = 2)
+                                json_metadata = json.dumps(metadata, indent = 2)
+    # Case 1a: Code is worth retrying
+                                if retry(code):
+                                  bucket.put_object(Body = json.dumps(json_error_data), Key = ('flagged_links/redirects/' + queue.home_domain + 'json'))
+    # Case 1b: Code is not worth retrying
+                                else:
+                                  bucket.put_object(Body = json.dumps(json_error_data), Key = ('flagged_links/error_links/' + queue.home_domain + '.json'))
+# Case 2: < 500 words found 
+                            elif queue.total_words < 500:
+                                status = "low_count"
+                                bucket.put_object(Body = json.dumps(json_data), Key = ('flagged_links/low_count/' + queue.home_domain + '.json'))
+                            else:
+# Case 3: Successful link
+                                bucket.put_object(Body = json.dumps(json_data), Key = ('html_data/' + queue.home_domain + '.json'))
                             message.delete()
-                            print("\n")
-                            print("\n")
+                            metadata = {
+                                    "domain": queue.root,
+                                    "response_code": code,
+                                    "word_count": queue.total_words,
+                                    "status": status
+                            }
+# Upload metadata
+                            json_string, current_bucket = get_metadata(queue.home_domain, metadata)
+# Add metadata to metadata/fail, remove from previous bucket if necessary
+                            if status == "fail":
+                                bucket.put_object(Body = json.dumps(json_metadata), Key = (f'metadata/fail/{queue.home_domain}.json'))
+                                if (current_bucket != "fail") and (current_bucket is not None):
+                                    s3.Object(BUCKET_NAME, (f'metadata/{current_bucket}/{queue.home_domain}.json')).delete()
+                            if status == "low_count":
+                                bucket.put_object(Body = json.dumps(json_metadata), Key = (f'metadata/low_count{queue.home_domain}.json'))
+                                if (current_bucket != "low_count") and (current_bucket is not None):
+                                    s3.Object(BUCKET_NAME, (f'metadata/{current_bucket}/{queue.home_domain}.json')).delete()
+                            else:
+                                bucket.put_object(Body = json.dumps(json_metadata), Key = (f'metadata/success/{queue.home_domain}.json'))
+                                if (current_bucket != "success") and (current_bucket is not None):
+                                    s3.Object(BUCKET_NAME, (f'metadata/{current_bucket}/{queue.home_domain}.json')).delete()
                         except Exception as scraper_error:
                             error_file.write(str(scraper_error))
                             check_files.append(url)
+                            error_data = {
+                                    "url": url,
+                                    "date": formatted_datetime
+                            }
+                            json_error_data = json.dumps(error_data, indent = 2)
+                            bucket.put_object(Body = json.dumps(json_error_data), Key = ('flagged_links/error_links/' + queue.home_domain + '.json'))
+                            message.delete() # remove bad link from queue, but save it in bucket
                     except Exception as sqs_error:
                         logging.error(f"An error with sqs occurred at {formatted_datetime}: {str(sqs_error)}\n\n\n\n\n", exc_info=True)
-                         
+                        error_data = {
+                                "url": url,
+                                "date": formatted_datetime
+                        }
+                        # procedures here to kill the process, recover server
+                        json_error_data = json.dumps(error_data, indent = 2)
+                        bucket.put_object(Body = json.dumps(json_error_data), Key = ('flagged_links/error_links/' + queue.home_domain + '.json'))
+                        if (len(url) > 0) and (message is not None):
+                            message.delete()
 
 
             end_time = time.time()
@@ -138,6 +234,9 @@ def main(sqs_name, number_links):
                 print(f"{key}, {value}")
     except Exception as e:
         logging.error(f"An error occurred at {formatted_datetime}: {str(e)}", exc_info=True)
+
+
+
         
 if __name__ == "__main__":
     number_links = sys.argv[1]
